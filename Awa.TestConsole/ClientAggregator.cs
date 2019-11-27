c29977913976fd4cb86374280d5ed6309e46dd1e
Darren E. Braynard <darren.braynard@mcdean.com>
2019-11-27 00:21:33 -0500

    initial commit

---
+++ Awa.TestConsole/ClientAggregator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awa.TestConsole {
+    class ClientAggregator {
+
+        public static List<double> ComputeRunningAverage(List<double> input,
+            int windowStart, int windowEnd, bool includeCurrentValue) {
+
+            var output = new List<double>();
+            var inputCount = input.Count;
+            var lastSum = 0d;
+            var lastWindowStartVal = 0d;
+            var lastWindowEndIndex = -1;
+            var startIndex = -windowEnd - 1;
+            for (var i = startIndex; i < inputCount; i++) {
+                var lookBackRaw = i - windowStart;
+                var lookBack = lookBackRaw;
+                var lookAheadRaw = i + windowEnd;
+                var lookAhead = lookAheadRaw >= inputCount
+                    ? inputCount - 1
+                    : lookAheadRaw;
+                var lastLookBackRaw = lookBack - 1;
+                var sum = lastSum;
+                if (lastLookBackRaw >= 0) {
+                    //remove the last lookBackRaw
+                    sum -= lastWindowStartVal;
+                }
+                //add the windowEnd to sum if window hasn't reached the end
+                if (lastWindowEndIndex < inputCount - 1 && lookAhead >= 0) {
+                    sum += input[lookAhead]; //windowEndVal
+                }
+                //only record values for inputs
+                if (i >= 0) {
+                    var lookBackForCount = lookBack < 0 ? 0 : lookBack;
+                    lastWindowStartVal = lookBack < 0 
+                        ? 0 
+                        : input[lookBack];
+                    var count = lookAhead - lookBackForCount + 1 +
+                                (includeCurrentValue ? 0 : -1);
+                    var currentValueAdj = !includeCurrentValue
+                        ? input[i]
+                        : 0;
+                    var sumForSaving = sum - currentValueAdj;
+                    var nextAvg = sumForSaving / count;
+                    output.Add(nextAvg);
+                }
+                lastSum = sum;
+                lastWindowEndIndex = lookAhead;
+            }
+            return output;
+        }
+    }
+}
---
+++ Awa.TestConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awa.TestConsole {
+    class Program {
+       
+        static void Main(string[] args) {
+
+            var input = GetSampleInput();
+
+            //TODO: Divide up this call into multiple threads
+            //TODO: to chunk the signal processing.
+            var output = ClientAggregator.ComputeRunningAverage(input,
+                3,
+                2,
+                false);
+
+            for (var i = 0; i < input.Count; i++) {
+                Console.WriteLine($"{Math.Round(input[i])},{Math.Round(output[i])}");
+            }
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+
+        private static List<double> GetUnitTestInput() {
+            var input = new List<double>();
+            for (var i = 0; i < 10; i++) {
+                input.Add(i + 1);
+            }
+
+            return input;
+        }
+
+        private static List<double> GetSampleInput() {
+            var list = new List<double> {
+                242.82,
+                112.38,
+                971.2,
+                915.62,
+                417.54,
+                616.74,
+                2807.46,
+                6248.02,
+                242.32,
+                2840.25,
+                6208.47,
+                231.21,
+                280.71,
+                2120.95,
+                222.04,
+                2787.33,
+                6232.34,
+                234.61,
+                574.22,
+                2025.31,
+                1816.08,
+                502.54,
+                2026.83,
+                1078.52,
+                316.17,
+                1574.83,
+                919.16,
+                2838.98,
+                1300.77
+            };
+
+            return list;
+        }
+    }
+}
